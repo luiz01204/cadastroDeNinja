8240e9f6f67373fa55420eabba332acfc5062c51
luiz01204 <luiz.machado.santos2000@gmail.com>
2025-10-24 21:34:47 -0300

    first commit

---
+++ src/main/java/br/dev/luizmachado/CadastrosDeNinja/CadastrosDeNinjaApplication.java
@@ -0,0 +1,13 @@
+package br.dev.luizmachado.CadastrosDeNinja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrosDeNinjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrosDeNinjaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/luizmachado/CadastrosDeNinja/CadastrosDeNinjaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.luizmachado.CadastrosDeNinja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrosDeNinjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
